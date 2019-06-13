9fa25efa4d737efd7a2a0b704d4be1da39c5ab94
Slizarenlook <43479126+Slizarenlook@users.noreply.github.com>
2019-06-13 15:59:08 +0300

    Update digispark_downloadboard.ino

--- digispark_downloadboard.ino
+++ digispark_downloadboard.ino
@@ -1,4 +1,6 @@
 #include "DigiKeyboard.h"
+#define MOD_SHIFT_LEFT      (1<<1)
+#define MOD_ALT_LEFT        (1<<2)
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,6 +8,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+delay(1000);
+DigiKeyboard.print("");
+delay(1000);
+DigiKeyboard.sendKeyStroke(MOD_SHIFT_LEFT, MOD_ALT_LEFT);
 delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
 delay(1000);
